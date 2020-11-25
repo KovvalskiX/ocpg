fcb464b8a6d89007a8ff3604e8103add5bec6893
KovvalskiX <kovvalskix@gmail.com>
2020-11-25 23:12:32 +0300

    Coded

--- oc.lua
+++ oc.lua
@@ -1,3 +1,6 @@
 local gpu = require("component").gpu
-local w,h = gpu.getResolution()
-while true do gpu.fill(math.random(0,w),math.random(0,h),math.random(1,3),math.random(1,3),tostring(math.random(0,1))) end
+local thread = require("thread")
+thread.create(function ()
+	local w,h = gpu.getResolution()
+	while true do gpu.fill(math.random(0,w),math.random(0,h),math.random(1,3),math.random(1,3),tostring(math.random(0,1))) end
+end).detach()
